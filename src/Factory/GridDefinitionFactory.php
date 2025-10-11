71214383226179ccf0296be426474604c389e959
theov <theo.vauvilliers10@gmail.com>
2025-10-11 11:56:17 +0200

    refac: Remove GridDefinitionDenormalizer class and adapt GridDefinitionFactory

--- src/Factory/GridDefinitionFactory.php
+++ src/Factory/GridDefinitionFactory.php
@@ -4,27 +4,30 @@ declare(strict_types=1);
 
 namespace TheoVauvilliers\GridBundle\Factory;
 
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use TheoVauvilliers\GridBundle\Entity\GridDefinition;
 use TheoVauvilliers\GridBundle\Provider\GridDefinitionProvider;
-use TheoVauvilliers\GridBundle\Serializer\DenormalizerInterface;
 use TheoVauvilliers\GridBundle\Transformer\GridDefinitionTransformer;
 
 readonly class GridDefinitionFactory
 {
     public function __construct(
         private GridDefinitionProvider $definitionProvider,
-        private DenormalizerInterface $denormalizer,
+        private DenormalizerInterface $serializer,
     ) {
     }
 
     /**
+     * @throws ExceptionInterface
      * @throws \Exception
      */
     public function create(string $name): GridDefinition
     {
         $definition = $this->definitionProvider->getDefinition($name);
         $definition = GridDefinitionTransformer::transformBeforeDenormalize($definition);
-        $definition = $this->denormalizer->denormalize($definition);
+
+        $definition = $this->serializer->denormalize($definition, GridDefinition::class);
 
         if (!$definition instanceof GridDefinition) {
             throw new \UnexpectedValueException(\sprintf('Unable to create "%s" for "%s".', GridDefinition::class, $name));
